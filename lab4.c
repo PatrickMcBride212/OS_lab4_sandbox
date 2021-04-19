2bf56b732cd6c4872992ffd390b9603e36859208
Patrick <mcbridep212@gmail.com>
2021-04-19 19:04:10 -0400

    a

--- lab4.c
+++ lab4.c
@@ -13,28 +13,24 @@
 #include <sys/uio.h>
 #include <unistd.h>
 #include <sys/stat.h>
-#include <stdbool.h>
 
 #define BACKLOG (10)
 
 static char const html_response[] = "HTTP/1.0 200 OK\r\n"
-  "Content-type: text/html; charset=UTF-8\r\n\r\n";
+                                    "Content-type: text/html; charset=UTF-8\r\n\r\n";
 static char const gif_response[] = "HTTP/1.0 200 OK\r\n"
-  "Content-type: image/gif\r\n\r\n";
-/*
+                                   "Content-type: image/gif\r\n\r\n";
 static char const jpeg_response[] = "HTTP/1.0 200 OK\r\n"
-  "Content-type: image/jpeg\r\n\r\n";
+                                    "Content-type: image/jpeg\r\n\r\n";
 static char const png_response[] = "HTTP/1.0 200 OK\r\n"
-  "Content-type: image/png\r\n\r\n";
+                                   "Content-type: image/png\r\n\r\n";
 static char const txt_response[] = "HTTP/1.0 200 OK\r\n"
-  "Content-type: text/plain\r\n\r\n";
+                                   "Content-type: text/plain\r\n\r\n";
 static char const pdf_response[] = "HTTP/1.0 200 OK\r\n"
-  "Content-type: application/pdf\r\n\r\n";
-static char const not_found_response[] = "HTTP/1.0 404 Not Found\r\n"
-  "Content-type: text/html; charset=UTF-8\r\n\r\n";
-static char const not_found_file[] = "./not_found.html";
-*/
-
+                                   "Content-type: application/pdf\r\n\r\n";
+//static char const not_found_response[] = "HTTP/1.0 404 Not Found\r\n"
+//"Content-type: text/html; charset=UTF-8\r\n\r\n";
+//static char const not_found_file[] = "./not_found.html";
 /* char* parseRequest(char* request)
  * Args: HTTP request of the form "GET /path/to/resource HTTP/1.X"
  *
@@ -44,99 +40,133 @@ static char const not_found_file[] = "./not_found.html";
  * Does not modify the given request string.
  * The returned resource should be free'd by the caller function.
  */
-/*
-static bool file_exists (char *filename) {
-  struct stat buffer;
-  return (stat (filename, &buffer) == 0);
-}
-*/
 
 static char *parseRequest(char *request) {
-  //assume file paths are no more than 4095 bytes + 1 for null.
-  char *buffer = calloc(4096, 1);
+    //assume file paths are no more than 4095 bytes + 1 for null.
+    char *buffer = calloc(4096, 1);
 
-  int version = 0;
-  char cr = 0;
-  char nl = 0;
-  if (sscanf(request, "GET %4096s HTTP/1.%d%c%c", buffer, &version, &cr, &nl) != 4
-      || cr != '\r' || nl != '\n')
-  {
-    free(buffer);
-    return 0;
-  }
+    int version = 0;
+    char cr = 0;
+    char nl = 0;
+    if (sscanf(request, "GET %4096s HTTP/1.%d%c%c", buffer, &version, &cr, &nl) != 4
+        || cr != '\r' || nl != '\n')
+    {
+        free(buffer);
+        return 0;
+    }
 
-  return buffer;
+    return buffer;
 }
 
 static void send_error(int client_fd, int http_status_code) {
-  char const *status = "";
-  switch (http_status_code) {
-  case 400:
-    status = "Bad Request";
-    break;
+    char const *status = "";
+    switch (http_status_code) {
+        case 400:
+            status = "Bad Request";
+            break;
 
-  case 404:
-    status = "Failure";
-    break;
-  }
-  char response[256];
-  snprintf(response, sizeof response, "HTTP/1.0 %d %s\r\nConnection: close\r\n\r\n",
-           http_status_code, status);
-  send(client_fd, response, strlen(response), 0);
+        case 404:
+            status = "Failure";
+            break;
+    }
+    char response[256];
+    snprintf(response, sizeof response, "HTTP/1.0 %d %s\r\nConnection: close\r\n\r\n",
+             http_status_code, status);
+    send(client_fd, response, strlen(response), 0);
 }
 
 static void serve_request(int client_fd, char * commandline_dir){
-  size_t offset = 0;
-  char buffer[4096] = { 0 };
+    size_t offset = 0;
+    char buffer[4096] = { 0 };
 
-  while (offset < sizeof buffer) {
-    ssize_t received = recv(client_fd, &buffer[offset],
-                            sizeof buffer - offset, 0);
-    if (strstr(buffer + offset,"\r\n\r\n"))
-      break;
-    offset += received;
-  }
+    while (offset < sizeof buffer) {
+        ssize_t received = recv(client_fd, &buffer[offset],
+                                sizeof buffer - offset, 0);
+        if (strstr(buffer + offset,"\r\n\r\n"))
+            break;
+        offset += received;
+    }
 
-  char *requested_file = parseRequest(buffer);
-  if (!requested_file) {
-    send_error(client_fd, 400); // Bad Request
-    return;
-  }
+    char *requested_file = parseRequest(buffer);
+    if (!requested_file) {
+        send_error(client_fd, 400); // Bad Request
+        return;
+    }
 
-  /* Make sure the requested_file starts with a / and does not contain /../
-   * anywhere.
-   */
-  if (requested_file[0] != '/' || strstr(requested_file, "/../")) {
-    send_error(client_fd, 400); // Bad Request
-    free(requested_file);
-    return;
-  }
-  //check for file type here. based off of file type, change response_str to proper header
+    /* Make sure the requested_file starts with a / and does not contain /../
+     * anywhere.
+     */
+    if (requested_file[0] != '/' || strstr(requested_file, "/../")) {
+        send_error(client_fd, 400); // Bad Request
+        free(requested_file);
+        return;
+    }
+    //check for file type here. based off of file type, change response_str to proper header
+    printf("file requested: %s\n", requested_file);
 
-  /*
-  printf("file requested: %s\n", requested_file);
-  char * temp = malloc(strlen(requested_file) + 1);
-  strcpy(temp, requested_file);
-  char * content_type = strtok(temp, ".");
-  content_type = strtok(NULL, ".");
-  printf("content type1: %s\n", content_type);
-  free(temp);
+    char * temp = malloc(strlen(requested_file) + 1);
+    strcpy(temp, requested_file);
+    char * content_type = strtok(temp, ".");
+    content_type = strtok(NULL, ".");
+    printf("content type: %s\n", content_type);
+
+    //first check for file existence. If file doesn't exist, send 404 error and return from current function
+    /*
+    if (access(requested_file, F_OK) != 0) {
+      //send the 404 error thing
+      printf("%s Not found\n", requested_file);
+      send(client_fd, not_found_response, sizeof(not_found_response)-1, 0);
+
+      char *file_path = malloc(strlen(not_found_file + 1));
+      strcpy(file_path, not_found_file);
+      printf("filepath: %s\n", file_path);
+      struct stat st;
+      stat(file_path, &st);
+      int size = st.st_size;
+      printf("filesize: %d\n", size);
+      int read_fd = open(file_path, O_RDONLY);
+      free(file_path);
+      ssize_t bytes_read = read(read_fd, buffer, sizeof buffer);
+      printf("read: %ld", bytes_read);
+      while (bytes_read != 0 && bytes_read != -1) {
+        int sent = send(client_fd, buffer, bytes_read, 0);
+        bytes_read = read(read_fd, buffer, sizeof buffer);
+        printf("sent: %d\n", sent);
+        printf("read: %ld", bytes_read);
+      }
+      printf("\n");
+      close(read_fd);
+
+      return;
+    }
   */
-
-  /*
-  char * file_check = malloc(strlen(requested_file + 1));
-  file_check[0] = '.';
-  strcpy(file_check+1, requested_file);
-  printf("filecheck: %s\n", file_check);
-  //first check for file existence. If file doesn't exist, send 404 error and return from current function
-  if (!file_exists(file_check)) {
-    printf("%s does not exist\n", requested_file);
-    //send the 404 error thing
-    //printf("%s Not found\n", requested_file);
-    send(client_fd, not_found_response, sizeof(not_found_response)-1, 0);
-    printf("%s\n", not_found_response);
-    char *file_path = malloc(strlen(not_found_file + 1));
-    strcpy(file_path, not_found_file);
+    //now check for file content type
+    //else
+    if (strcmp(content_type, "pdf") == 0) {
+        send(client_fd, pdf_response, sizeof(pdf_response)-1, 0);
+    }
+    else if(strcmp(content_type, "png") == 0){
+        send(client_fd, png_response, sizeof(png_response)-1, 0);
+    }
+    else if(strcmp(content_type, "gif") == 0){
+        send(client_fd, gif_response, sizeof(gif_response)-1, 0);
+    }
+    else if(strcmp(content_type, "txt") == 0){
+        send(client_fd, txt_response, sizeof(txt_response)-1, 0);
+    }
+    else if(strcmp(content_type, "jpeg") == 0){
+        send(client_fd, jpeg_response, sizeof(jpeg_response)-1, 0);
+    }
+    else {
+        send(client_fd, html_response, sizeof(html_response)-1, 0);
+    }
+    //send(client_fd, response, sizeof(response) - 1, 0);
+    //printf("Command line dir: %s\n", commandline_dir);
+    // take requested_file, add a . to beginning, open that file
+    char *file_path = malloc(strlen(requested_file) + 2);
+    file_path[0] = '.';
+    strcpy(file_path + 1, requested_file);
+    free(requested_file);
     printf("filepath: %s\n", file_path);
     struct stat st;
     stat(file_path, &st);
@@ -145,83 +175,16 @@ static void serve_request(int client_fd, char * commandline_dir){
     int read_fd = open(file_path, O_RDONLY);
     free(file_path);
     ssize_t bytes_read = read(read_fd, buffer, sizeof buffer);
-    printf("read: %ld", bytes_read);
-    while (bytes_read != 0 && bytes_read != -1) {
-      int sent = send(client_fd, buffer, bytes_read, 0);
-      printf("%s\n", buffer);
-      bytes_read = read(read_fd, buffer, sizeof buffer);
-      printf("sent: %d\n", sent);
-      printf("read: %ld", bytes_read);
+    printf("read: %ld ", bytes_read);
+    while(bytes_read != 0 && bytes_read != -1){
+        int sent = send(client_fd, buffer, bytes_read, 0);
+        bytes_read = read(read_fd, buffer, sizeof buffer);
+        printf("sent: %d\n", sent);
+        printf("read: %ld ", bytes_read);
+        //send(client_fd, buffer, bytes_read, 0);
     }
     printf("\n");
     close(read_fd);
-    
-    return;
-  }
-  */
-  //now check for file content type
-  printf("file requested: %s\n", requested_file);
-  char * temp = malloc(strlen(requested_file) + 1);
-  strcpy(temp, requested_file);
-  char * content_type = strtok(temp, ".");
-  content_type = strtok(NULL, ".");
-  printf("content type: %s\n", content_type);
-  free(temp);
-  /*
-  if (strcmp(content_type, "pdf") == 0) {
-    printf("pdf header sent\n");
-    send(client_fd, pdf_response, sizeof(pdf_response)-1, 0);
-  }
-  else if(strcmp(content_type, "png") == 0){
-    printf("png header sent\n");
-    send(client_fd, png_response, sizeof(png_response)-1, 0);
-  }
-  else if(strcmp(content_type, "gif") == 0){
-    printf("gif header sent\n");
-    send(client_fd, gif_response, sizeof(gif_response)-1, 0);
-  }
-  else if(strcmp(content_type, "txt") == 0){
-    printf("txt header sent\n");
-    send(client_fd, txt_response, sizeof(txt_response)-1, 0);
-  }
-  else if(strcmp(content_type, "jpg") == 0 || strcmp(content_type, "jpeg") == 0) {
-    printf("jpg header sent\n");
-    send(client_fd, jpeg_response, sizeof(jpeg_response)-1, 0);
-  } else if {
-    printf("html header sent\n");
-    send(client_fd, html_response, sizeof(html_response)-1, 0);
-  }
-  */
-  if (strcmp(content_type, "gif") == 0) {
-    printf("gif\n");
-    send(client_fd, gif_response, sizeof(gif_response)-1, 0);
-  } else {
-    printf("html\n");
-    send(client_fd, html_response, sizeof(html_response)-1, 0);
-  }
-  // take requested_file, add a . to beginning, open that file
-  char *file_path = malloc(strlen(requested_file) + 2);
-  file_path[0] = '.';
-  strcpy(file_path + 1, requested_file);
-  free(requested_file);
-  printf("filepath: %s\n", file_path);
-  struct stat st;
-  stat(file_path, &st);
-  int size = st.st_size;
-  printf("filesize: %d\n", size);
-  int read_fd = open(file_path, O_RDONLY);
-  free(file_path);
-  ssize_t bytes_read = read(read_fd, buffer, sizeof buffer);
-  printf("read: %ld ", bytes_read);
-  while(bytes_read != 0 && bytes_read != -1){
-    int sent = send(client_fd, buffer, bytes_read, 0);
-    bytes_read = read(read_fd, buffer, sizeof buffer);
-    printf("sent: %d\n", sent);
-    printf("read: %ld ", bytes_read);
-    //send(client_fd, buffer, bytes_read, 0);
-  }
-  printf("\n");
-  close(read_fd);
 }
 
 /* Your program should take two arguments:
@@ -262,8 +225,8 @@ int main(int argc, char **argv) {
     opt = 0;
     retval = setsockopt(server_sock, IPPROTO_IPV6, IPV6_V6ONLY, &opt, sizeof opt);
     if (retval < 0) {
-      perror("Setting socket option IPV6_V6ONLY failed");
-      exit(1);
+        perror("Setting socket option IPV6_V6ONLY failed");
+        exit(1);
     }
 
     /* Create an address structure.  This is very similar to what we saw on the
@@ -295,9 +258,9 @@ int main(int argc, char **argv) {
     printf("chdir retval: %d\n", chdir_retval);
     char cwd[256];
     if (getcwd(cwd, sizeof(cwd)) == NULL) {
-      perror("getcwd() error\n");
+        perror("getcwd() error\n");
     } else {
-      printf("current working directory: %s\n", cwd);
+        printf("current working directory: %s\n", cwd);
     }
     /* Now that we've bound to an address and port, we tell the OS that we're
      * ready to start listening for client connections.  This effectively
