f4220a3fb1e9ca3bc13876bb22cab3267ee44a91
Patrick <mcbridep212@gmail.com>
2021-04-19 20:32:21 -0400

    s

--- lab4.c
+++ lab4.c
@@ -125,17 +125,18 @@ static void serve_request(int client_fd, char * commandline_dir){
         send(client_fd, html_response, sizeof(html_response)-1, 0);
         char * final_path;
         int base_length = (int) strlen(file_path);
+        int full_length;
         if (file_path[strlen(file_path)-1] == '/') {
             int add = (int) strlen("index.html");
             printf("%s ends in slash\n", file_path);
             final_path = malloc(strlen(file_path) + add + 1);
-            //full_length = (int) (strlen(file_path) + add + 1);
+            full_length = (int) (strlen(file_path) + add + 1);
             strcpy(final_path, file_path);
         } else {
             int add = (int) strlen("index.html");
             printf("%s doesn't end in slash\n", file_path);
             final_path = malloc(strlen(file_path) + add + 1);
-            //full_length = (int) (strlen(file_path) + add + 1);
+            full_length = (int) (strlen(file_path) + add + 1);
             strcpy(final_path, file_path);
             printf("insert at index %d\n", base_length);
             final_path[base_length] = '/';
@@ -144,6 +145,7 @@ static void serve_request(int client_fd, char * commandline_dir){
         printf("final path base: %s\n", final_path);
         strcpy(final_path+base_length, reference);
         printf("full path: %s\n", final_path);
+        printf("full path length: %d\n", full_length);
     } else {
         printf("%s isn't a dir\n", file_path);
     }
