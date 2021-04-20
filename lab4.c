1bd4c28a04f9198410209b879d3b26078ac6b82b
Patrick <mcbridep212@gmail.com>
2021-04-19 20:33:46 -0400

    more strings

--- lab4.c
+++ lab4.c
@@ -140,6 +140,7 @@ static void serve_request(int client_fd, char * commandline_dir){
             strcpy(final_path, file_path);
             printf("insert at index %d\n", base_length);
             final_path[base_length] = '/';
+            base_length++;
         }
         printf("base length: %d\n", base_length);
         printf("final path base: %s\n", final_path);
